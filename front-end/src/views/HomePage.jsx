381ee40a04d5629609a6e6dd71b66896c9a88652
Regeln <regeln17@protonmail.com>
2024-09-28 17:19:30 +0300

    Implement data fetching from backend, add events and pagination to UI

--- front-end/src/views/HomePage.jsx
+++ front-end/src/views/HomePage.jsx
@@ -1,18 +1,58 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "../index.css";
-import Event from "../components/Event.jsx";
 
 
 function HomePage() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageCount, setPageCount] = useState();
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const loadEvents = async () => {
+            const res = await fetch(`http://localhost:4000/api/events?page=${currentPage}&pageSize=12`);
+            const data = await res.json();
+            setPageCount(data.pageCount);
+            setEvents(data.events);
+        };
+
+        loadEvents();
+    }, [currentPage]);   
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < pageCount) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
     return (
         <>
             <h2>Events</h2>
             <div className="grid_wrapper">
-                <Event />
-                <Event />                         
+                {events.map(event => 
+                <div key={event.id} className="event_component">
+                    <h3>{event.title}</h3>
+                    <p>{event.description}</p>
+                    <p>{event.date}</p>
+                    <p>{event.organiser}</p>
+                    <Link to="/register">Register</Link>
+                    <Link to="/participants" className="view">View</Link>
+                </div>
+                )}
             </div>
-            
+            <button onClick={handlePrevPage}>
+                ←
+            </button>
+            <button onClick={handleNextPage}>
+                →
+            </button>
         </>
     );
 }
