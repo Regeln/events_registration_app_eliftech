8590439102595f0011bcb268c4cf7fb1179deda0
Regeln <regeln17@protonmail.com>
2024-09-28 22:27:09 +0300

    Create participants page

--- front-end/src/views/ParticipantsPage.jsx
+++ front-end/src/views/ParticipantsPage.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import "../index.css";
+
 function ParticipantsPage() {
+    const params = useParams();
+
+    const [participants, setParticipants] = useState([]);
+    const [eventName, setEventName] = useState("");
+
+    useEffect(() => {
+        const loadParticipants = async () => { 
+            const res = await fetch(`http://localhost:4000/api/events/${params.eventId}/participants`);
+            const data = await res.json();
+            
+            setParticipants(data);
+            
+        };
+
+        loadParticipants();
+    }, [params.id])  
+
     return (
         <div>
-            <p>Participants Page</p>
+            <h2>"{eventName}" participants</h2>
+            <div className="grid-wrapper-participants">
+                {participants.map(participant => 
+                    <div key={participant.id} className="participant-component">
+                        <h3>{participant.name}</h3>
+                        <p>{participant.email}</p>
+                    </div>
+                    )}
+            </div>
         </div>
     );
 }
